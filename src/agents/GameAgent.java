05cd649a218e164bb59b707a1eb76bc91d012aee
tripor <figorifico3000@gmail.com>
2019-11-07 11:51:46 +0000

    não sei

--- src/agents/GameAgent.java
+++ src/agents/GameAgent.java
@@ -4,11 +4,6 @@ import jade.core.Agent;
 
 public abstract class GameAgent extends Agent {
 
-    public void setup()
-    {
-        //Comunicar o game controller da minha existencia para me ser atribuida uma cidade
-        start();
-    }
-    public abstract void start();
+    public abstract void setup();
     public abstract void takeDown();
 }
