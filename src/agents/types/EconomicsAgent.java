d8abcb79023d275d2fe7313cd2afaf83183bd57b
tripor <figorifico3000@gmail.com>
2019-11-17 20:14:15 +0000

    changed money in economist

--- src/agents/types/EconomicsAgent.java
+++ src/agents/types/EconomicsAgent.java
@@ -21,26 +21,28 @@ public class EconomicsAgent extends GameAgent {
         this.moneyToBuyEmptyCities = this.currentMoney;
         my_new_cities = buyEmptyCities(my_new_cities);
 
-        // Distribute the remaining money
-        this.moneyToUpgrade = this.currentMoney / 4;
-        this.moneyToAttack = this.moneyToUpgrade;
-        this.moneyToDefenses = this.moneyToUpgrade;
-        this.moneyToReligion = this.moneyToUpgrade;
-        this.currentMoney = 0;
-
         // Defend their cities from religious attacks
+        this.moneyToReligion = this.currentMoney / 4;
+        this.currentMoney -= this.moneyToReligion;
         defendReligion();
         this.currentMoney += this.moneyToReligion;
 
+
         // Buy opponents cities
+        this.moneyToAttack = this.currentMoney / 4;
+        this.currentMoney -= this.moneyToAttack;
         my_new_cities = buyOpponentsCities(my_new_cities);
         this.currentMoney += this.moneyToAttack;
 
         // Upgrade their defenses
+        this.moneyToDefenses = this.currentMoney / 4;
+        this.currentMoney -= this.moneyToDefenses;
         this.upgradeMyDefenses();
         this.currentMoney += this.moneyToDefenses;
 
         // Upgrade their cities
+        this.moneyToUpgrade = this.currentMoney / 4;
+        this.currentMoney -= this.moneyToUpgrade;
         upgradeCities();
         this.currentMoney += this.moneyToUpgrade;
 
